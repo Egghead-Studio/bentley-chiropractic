69c140eb6f580a38936f36aa6dd2bdf9a17a77f5
stephen <kernan.software@gmail.com>
2024-05-26 15:11:52 -0500

    EGG-16: Add Color Palette to Tailwind Config

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,7 +1,7 @@
 'use client'
-import React from 'react';
-import Link from "next/link";
-import {Menu} from "react-feather";
+import React from 'react'
+import Link from 'next/link'
+import { Menu } from 'react-feather'
 
 
 interface NavProps {
@@ -9,7 +9,6 @@ interface NavProps {
 }
 
 export const Nav: React.FC<NavProps> = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
   const navLinks = [
     {
       name: 'Services',
@@ -23,28 +22,29 @@ export const Nav: React.FC<NavProps> = () => {
       name: 'Testimonials',
       url: '/testimonials'
     }
-  ];
+  ]
   return (
     <nav>
-      <div className="drawer bg-secondary">
+      <div className="drawer drawer-end bg-background">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content flex items-center bg-secondary py-2 px-4 md:px-0 md:mx-auto md:w-10/12 lg:w-9/12">
-          <div className={'text-2xl'}>LOGO</div>
-
+        <div className="drawer-content flex items-center py-2 px-4 md:px-0 md:mx-auto md:w-10/12 lg:w-9/12">
+          <a href={'/'}>
+            <div className={'text-3xl font-heading font-bold'}>Bentley Chiropractic</div>
+          </a>
           <label htmlFor="my-drawer" className="btn btn-ghost drawer-button ml-auto"><Menu/></label>
         </div>
         <div className="drawer-side">
-          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay" />
+          <ul className="menu p-4 w-80 min-h-full bg-background text-primary-text">
             {/* Sidebar content here */}
             {navLinks.map((navLink) => (
               <li key={navLink.name}>
                 <a href={navLink.url}> {navLink.name} </a>
               </li>
-              ))}
+            ))}
           </ul>
         </div>
       </div>
     </nav>
-  );
+  )
 }
