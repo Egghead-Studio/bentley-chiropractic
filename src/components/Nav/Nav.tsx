969e2ed35c21dc99f6f7e1aefe4ecaf1e180a277
stephen <kernan.software@gmail.com>
2024-05-28 19:27:51 -0500

    EGG-18: Update Nav Links

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -10,22 +10,35 @@ interface NavProps {
 
 }
 
-export const Nav: React.FC<NavProps> = () => {
-  const { getEventProperties } = useContext(AnalyticsContext)
+const NavLinks: React.FC = () => {
   const navLinks = [
-    {
-      name: 'Services',
-      url: '/services'
-    },
     {
       name: 'About',
       url: '/about'
     },
     {
-      name: 'Testimonials',
-      url: '/testimonials'
-    }
+      name: 'Services',
+      url: '/services'
+    },
+    // Won't have this page until we have a significant number of reviews
+    // {
+    //   name: 'Testimonials',
+    //   url: '/testimonials'
+    // }
   ]
+
+  return (
+    navLinks.map((navLink) => (
+      <li key={navLink.name}>
+        <a href={navLink.url}>{navLink.name}</a>
+      </li>
+    ))
+  )
+}
+
+
+export const Nav: React.FC<NavProps> = () => {
+  const { getEventProperties } = useContext(AnalyticsContext)
 
   useEffect(() => {
     const eventProps = getEventProperties()
@@ -40,17 +53,22 @@ export const Nav: React.FC<NavProps> = () => {
           <a href={'/'}>
             <div className={'text-3xl font-heading font-bold'}>Bentley Chiropractic</div>
           </a>
-          <label htmlFor="my-drawer" className="btn btn-ghost drawer-button ml-auto"><Menu/></label>
+          <div className={'hidden lg:flex gap-4 ml-auto list-none'}>
+            <NavLinks />
+            <li>
+              <a href={'https://maps.app.goo.gl/iKxuYJpq1yQLiKXn7'} target={'_blank'}>Location</a>
+            </li>
+          </div>
+          <label htmlFor="my-drawer" className="btn btn-ghost drawer-button ml-auto lg:hidden"><Menu/></label>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay" />
           <ul className="menu p-4 w-80 min-h-full bg-background text-blackbeard">
             {/* Sidebar content here */}
-            {navLinks.map((navLink) => (
-              <li key={navLink.name}>
-                <a href={navLink.url}>{navLink.name}</a>
-              </li>
-            ))}
+            <NavLinks />
+            <li>
+              <a href={'https://maps.app.goo.gl/iKxuYJpq1yQLiKXn7'} target={'_blank'}>Location</a>
+            </li>
           </ul>
         </div>
       </div>
