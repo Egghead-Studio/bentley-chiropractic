b0aaf4f21c3e182196d9856cc146a46a4e8aeccf
stephen <kernan.software@gmail.com>
2024-05-29 21:30:41 -0500

    EGG-14: Update Headers

--- src/components/FAQ/FAQ.tsx
+++ src/components/FAQ/FAQ.tsx
@@ -24,7 +24,7 @@ export const FAQSection: React.FC = () => {
 
   return (
     <div className={'w-page my-12 lg:my-24 flex flex-col gap-8 md:gap-12'}>
-      <div className={'text-primary text-4xl text-center font-bold font-heading'}>FAQ</div>
+      <div className={'text-primary text-4xl text-center font-bold font-heading'}>Frequently asked questions</div>
       <div className={'flex flex-col gap-4'}>
         {questions.map((q: Question, index: number) => (
           <QuestionAccordion key={`question-${index}`} question={q} />
