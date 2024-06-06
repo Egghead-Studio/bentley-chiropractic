e55813a7f23fa737c8515078dc007a6d8349905c
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2024-06-06 18:45:54 -0500

    Fixed image path

--- src/components/MainSection/MainSection.tsx
+++ src/components/MainSection/MainSection.tsx
@@ -10,7 +10,7 @@ interface MainSectionProps {
   headerText?: string
   buttonText?: string
   items?: SectionText[]
-  imagePath?: string | StaticImport
+  imagePath: string | StaticImport
 }
 
 export const MainSection: React.FC<MainSectionProps> = ({ imagePath, items }) => {
@@ -18,7 +18,7 @@ export const MainSection: React.FC<MainSectionProps> = ({ imagePath, items }) =>
   return (
     <div className={'flex w-page gap-12 items-center py-12 lg:py-24'}>
       <div className={'flex flex-col gap-4'}>
-        <Image className={'shadow-xl rounded-lg w-full mb-4 md:mb-8'} src={'/headache.jpg'} alt={'Dr. Matthew Bentley'} width={200} height={200} />
+        <Image className={'shadow-xl rounded-lg w-full mb-4 md:mb-8'} src={imagePath} alt={'Dr. Matthew Bentley'} width={200} height={200} />
         {items && items.map((item, index) => (
           <TextBlock type={item.type} key={'item'+index}>
             {item.text}
