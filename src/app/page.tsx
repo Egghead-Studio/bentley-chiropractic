b6255461fb7404080303357567fe3b4d6d1afe22
stephen <kernan.software@gmail.com>
2024-06-15 08:06:16 -0500

    Update meta tags and add preview screenshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,35 @@ import { PatientTestimonials } from '@/components/PatientTestimonials/PatientTes
 import { WhyGonsteadWorks } from '@/components/WhyGonsteadWorks/WhyGonsteadWorks'
 import { AboutMatt } from '@/components/AboutMatt/AboutMatt'
 import { sendEvent } from '@/events/events'
+import { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   title: 'Home | Bentley Chiropractic',
+  description: 'Bentley Chiropractic gives the body what it needs to function to its fullest potential. We don\'t just mask the problem - we aim find the root cause of your pain and dysfunction so we can provide a natural alternative to health care.',
+  keywords: ['chiropractor', 'Dr. Matthew Bentley', 'health care', 'natural health', 'increased mobility', 'headaches', 'injury recovery', 'gonstead chiropractor', 'chiropractors in stl', 'saint louis', 'st louis'],
+  icons: {
+    icon: '/bentley-logo-no-bg.png',
+    shortcut: '/bentley-logo-no-bg.png',
+    apple: '/bentley-logo-no-bg.png',
+    other: {
+      rel: '/bentley-logo-no-bg.png',
+      url: '/bentley-logo-no-bg.png',
+    }
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Bentley Chiropractic | Book online for 20% off your first visit',
+    description: 'Bentley Chiropractic gives the body what it needs to function to its fullest potential. We don\'t just mask the problem - we aim find the root cause of your pain and dysfunction so we can provide a natural alternative to health care.',
+    images: 'https://www.bentleychiropracticstl.com/preview-screenshot.png',
+  },
+  openGraph: {
+    title: 'Bentley Chiropractic | Book online for 20% off your first visit',
+    siteName: 'Bentley Chiropractic',
+    url: 'https://bentleychiropracticstl.com',
+    locale: 'en_US',
+    type: 'website',
+    images: 'https://www.bentleychiropracticstl.com/preview-screenshot.png',
+  }
 }
 
 export default async function Home() {
